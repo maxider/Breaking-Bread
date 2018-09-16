a8eddfeb4943a10f4d086eb16b47c1c63ecfdf77
Max Haas <maxweilandt1998@gmail.com>
2018-09-16 19:11:06 +0200

    InDev 3

---
+++ Assets/Scripts/Instantiation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiation : MonoBehaviour {
+
+    public Camera cam;
+
+    public GameObject ShopScreenItems;
+
+    private void Awake()
+    {
+        placeShopScreen();
+    }
+
+    void placeShopScreen()
+    {
+        Vector3 oldPos = ShopScreenItems.transform.position;
+        Vector3 newPos = cam.ScreenToWorldPoint(new Vector3(Screen.width, ShopScreenItems.transform.position.y, ShopScreenItems.transform.position.z));
+        newPos.z = oldPos.z;
+        newPos.y = oldPos.y;
+        ShopScreenItems.transform.position = newPos;
+    }
+}
